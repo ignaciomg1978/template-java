23ac2220bef2d7de45b28fdff9fd6a168778eb10
ignaciomg1978 <78687720+ignaciomg1978@users.noreply.github.com>
2025-01-23 13:09:23 +0100

    Update DemoApplicationTests.java
    añadido el entryPointTest()

--- src/test/java/com/softtek/demo/DemoApplicationTests.java
+++ src/test/java/com/softtek/demo/DemoApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -15,6 +17,11 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 		assertNotNull(applicationContext);
+	}
+
+	@Test
+	void entryPointTest(){
+		assertDoesNotThrow(() -> DemoApplication.main(new String[]{}));
 	}
 
 }
