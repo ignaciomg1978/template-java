6070380ebecb4e8e78ecfbc34734c24b6f010ef8
ignaciomg1978 <78687720+ignaciomg1978@users.noreply.github.com>
2025-01-23 13:03:53 +0100

    Update DemoApplicationTests.java
    Corregido el test

--- src/test/java/com/softtek/demo/DemoApplicationTests.java
+++ src/test/java/com/softtek/demo/DemoApplicationTests.java
@@ -2,12 +2,19 @@ package com.softtek.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(applicationContext);
 	}
 
 }
