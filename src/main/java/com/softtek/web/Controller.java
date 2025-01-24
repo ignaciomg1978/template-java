0d5068aebcb3a4cc4571da13094fd8f5273fc912
ignaciomg1978 <78687720+ignaciomg1978@users.noreply.github.com>
2025-01-24 14:03:24 +0100

    Create Controller.java

---
+++ src/main/java/com/softtek/web/Controller.java
@@ -0,0 +1,18 @@
+package com.softtek.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@Tag(name = "My first API", description = "Exposes endpoints to interact ...")
+public class Controller {
+
+    @Operation(summary = "Saludo", description = "Saluda a todo el mundo")
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
